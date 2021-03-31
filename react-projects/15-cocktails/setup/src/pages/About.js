2a10153f277594cc4308f8cc9931d139c7f5cb89
Santiago Yanguas <santitoya-2001@outlook.com>
2021-03-30 21:02:59 -0500

    [feature] react projects

---
+++ react-projects/15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus
+        libero id posuere maximus. Integer rutrum turpis lectus, at consequat
+        magna pretium ut. Proin pellentesque nunc at fermentum rutrum. Donec
+        mattis consectetur euismod. Mauris nulla lectus, fermentum vitae
+        eleifend et, sodales a nibh. Curabitur tincidunt dui eget condimentum
+        egestas. Pellentesque varius lorem non magna tincidunt porta. Duis
+        mattis ante lorem, a egestas velit ornare at. Mauris nec tortor non
+        lacus tincidunt vulputate quis in lacus. Curabitur a risus ut purus
+        interdum suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Maecenas suscipit, magna vitae finibus imperdiet, felis orci
+        interdum massa, vulputate fringilla magna mi in erat. Nulla vitae congue
+        est, quis placerat sem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ react-projects/5-menu/setup/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = [
+  "all",
+  ...new Set(
+    items.map((item) => {
+      return item.category;
+    })
+  ),
+];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => {
+      return item.category === category;
+    });
+
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu-section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={allCategories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
